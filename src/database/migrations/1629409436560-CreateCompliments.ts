4fa8a61c1d87e8c9deadcf60481b314e2570ac7b
Gabriel Torres Brum <gabriel.torres@3csistemas.com.br>
2021-08-20 10:35:02 -0300

    Aula 04 finalizada

--- src/database/migrations/1629409436560-CreateCompliments.ts
+++ src/database/migrations/1629409436560-CreateCompliments.ts
@@ -1,9 +1,9 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
 export class CreateCompliments1629409436560 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable{
+        await queryRunner.createTable(
             new Table({
                 name: "compliments",
                 columns: [
@@ -23,13 +23,60 @@ export class CreateCompliments1629409436560 implements MigrationInterface {
                     {
                         name: "tag_id",
                         type: "uuid",
+                    },
+                    {
+                        name: "message",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
                     }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKUserSenderCompliments",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_sender"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKUserReceiverCompliments",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_receiver"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKTagCompliments",
+                        referencedTableName: "tags",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["tag_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
                 ]
             })
-        }
+        )
+        // await queryRunner.createForeignKey(
+        //     "compliments",
+        //     new TableForeignKey({
+        //         name: "FKUserSenderCompliments",
+        //         referencedTableName: "users",
+        //         referencedColumnNames: ["id"],
+        //         columnNames: ["user_sender"],
+        //         onDelete: "SET NULL",
+        //         onUpdate: "SET NULL"
+        //     })
+        // )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("compliments");
     }
 
 }
